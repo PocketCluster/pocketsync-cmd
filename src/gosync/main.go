74526c1ca66d1b19a93e441722e11559ecc78078
stkim1 <stkim1@colorfulglue.com>
2017-09-05 21:42:18 +0900

    fix indent

--- src/gosync/main.go
+++ src/gosync/main.go
@@ -5,61 +5,61 @@ but supposed to be functional in itself.
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"os"
-	"runtime"
+    "fmt"
+    "log"
+    "net/http"
+    _ "net/http/pprof"
+    "os"
+    "runtime"
 
-	"github.com/codegangsta/cli"
+    "github.com/codegangsta/cli"
 )
 
 const (
-	DefaultBlockSize = 8192
-	magicString      = "G0S9NC" // just to confirm the file type is used correctly
-	majorVersion     = uint16(0)
-	minorVersion     = uint16(2)
-	patchVersion     = uint16(1)
+    DefaultBlockSize = 8192
+    magicString      = "G0S9NC" // just to confirm the file type is used correctly
+    majorVersion     = uint16(0)
+    minorVersion     = uint16(2)
+    patchVersion     = uint16(1)
 )
 
 var app = cli.NewApp()
 
 func main() {
-	app.Name = "gosync"
-	app.Usage = "Build indexes, patches, patch files"
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "profile",
-			Usage: "enable HTTP profiling",
-		},
-		cli.IntFlag{
-			Name:  "profilePort",
-			Value: 6060,
-			Usage: "The number of streams to use concurrently",
-		},
-	}
+    app.Name = "gosync"
+    app.Usage = "Build indexes, patches, patch files"
+    app.Flags = []cli.Flag{
+        cli.BoolFlag{
+            Name:  "profile",
+            Usage: "enable HTTP profiling",
+        },
+        cli.IntFlag{
+            Name:  "profilePort",
+            Value: 6060,
+            Usage: "The number of streams to use concurrently",
+        },
+    }
 
-	app.Version = fmt.Sprintf(
-		"%v.%v.%v",
-		majorVersion,
-		minorVersion,
-		patchVersion,
-	)
+    app.Version = fmt.Sprintf(
+        "%v.%v.%v",
+        majorVersion,
+        minorVersion,
+        patchVersion,
+    )
 
-	runtime.GOMAXPROCS(runtime.NumCPU())
+    runtime.GOMAXPROCS(runtime.NumCPU())
 
-	app.Before = func(c *cli.Context) error {
-		if c.Bool("profile") {
-			port := fmt.Sprint(c.Int("profilePort"))
+    app.Before = func(c *cli.Context) error {
+        if c.Bool("profile") {
+            port := fmt.Sprint(c.Int("profilePort"))
 
-			go func() {
-				log.Println(http.ListenAndServe("localhost:"+port, nil))
-			}()
-		}
+            go func() {
+                log.Println(http.ListenAndServe("localhost:"+port, nil))
+            }()
+        }
 
-		return nil
-	}
+        return nil
+    }
 
-	app.Run(os.Args)
+    app.Run(os.Args)
 }
