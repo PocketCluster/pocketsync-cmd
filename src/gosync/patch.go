370b530b249ffc3ef58180a31c58b2e53458e33b
stkim1 <stkim1@colorfulglue.com>
2017-09-06 22:30:16 +0900

    multi source patcher

--- src/gosync/patch.go
+++ src/gosync/patch.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+    "bufio"
     "os"
-    "runtime"
+    "time"
 
     log "github.com/Sirupsen/logrus"
     "github.com/pkg/errors"
-    gosync_main "github.com/Redundancy/go-sync"
+    "github.com/Redundancy/go-sync/blockrepository"
+    "github.com/Redundancy/go-sync/blocksources"
+    "github.com/Redundancy/go-sync/filechecksum"
+    "github.com/Redundancy/go-sync/patcher"
+    "github.com/Redundancy/go-sync/patcher/multisources"
     "github.com/codegangsta/cli"
 )
 
-const usage = "gosync patch <localfile> <reference index> <reference source> [<output>]"
+const usage = "gosync patch <reference index> <reference repository list> <output>"
 
 func init() {
     app.Commands = append(
@@ -22,17 +27,10 @@ func init() {
             Description: `Recreate the reference source file, using an index and a local file that is believed to be similar.
 The index should be produced by "gosync build".
 
-<reference index> is a .gosync file and may be a local, unc network path or http/https url
-<reference source> is corresponding target and may be a local, unc network path or http/https url
-<output> is optional. If not specified, the local file will be overwritten when done.`,
+<reference index> is a .gosync file and may be a local, unc network path or http/https url.
+<reference repository list> is corresponding repository list in .text file format.
+<output> is the local file will be overwritten when done.`,
             Action: Patch,
-            Flags: []cli.Flag{
-                cli.IntFlag{
-                    Name:  "p",
-                    Value: runtime.NumCPU(),
-                    Usage: "The number of streams to use concurrently",
-                },
-            },
         },
     )
 }
@@ -42,58 +40,95 @@ func Patch(c *cli.Context) {
     errorWrapper(c, func(c *cli.Context) error {
 
         log.Println("Starting patching process")
-        if l := len(c.Args()); l < 3 || l > 4 {
+        if len(c.Args()) < 3 {
             return errors.Errorf("Usage is \"%v\" (invalid number of arguments)", usage)
         }
-
         var (
-            localFilename = c.Args()[0]
-            outFilename   = c.Args()[0]
-            summaryFile   = c.Args()[1]
-            referencePath = c.Args()[2]
+            refIndexName  = c.Args()[0]
+            refListName   = c.Args()[1]
+            outFileName   = c.Args()[2]
         )
-        if len(c.Args()) == 4 {
-            outFilename = c.Args()[3]
+        if len(refIndexName) == 0 {
+            return errors.Errorf("Usage is \"%v\" (invalid reference index filename)", usage)
+        }
+        if len(refListName) == 0 {
+            return errors.Errorf("Usage is \"%v\" (invalid reference repository list filename)", usage)
+        }
+        if len(outFileName) == 0 {
+            return errors.Errorf("Usage is \"%v\" (invalid output filename)", usage)
         }
 
-        indexReader, err := os.Open(summaryFile)
+        // index file
+        indexReader, err := os.Open(refIndexName)
         if err != nil {
             return errors.WithStack(err)
         }
-        defer indexReader.Close()
+        // read repository list
+        refListReader, err := os.Open(refListName)
+        if err != nil {
+            return errors.WithStack(err)
+        }
+        // otuput file
+        outFile, err := os.Create(outFileName)
+        if err != nil {
+            return errors.WithStack(err)
+        }
+        defer func() {
+            indexReader.Close()
+            refListReader.Close()
+            outFile.Close()
+        }()
 
+        // read index & build checksum
         filesize, blocksize, blockcount, rootHash, err := readHeadersAndCheck(indexReader)
         if err != nil {
             return errors.WithStack(err)
         }
-
         index, checksumLookup, err := readIndex(indexReader, uint(blocksize), uint(blockcount), rootHash)
         if err != nil {
             return errors.WithStack(err)
         }
 
-        fs := &gosync_main.BasicSummary{
-            ChecksumIndex:  index,
-            ChecksumLookup: checksumLookup,
-            BlockCount:     uint(blockcount),
-            BlockSize:      uint(blocksize),
-            FileSize:       filesize,
-        }
-        rsync, err := gosync_main.MakeRSync(
-            localFilename,
-            referencePath,
-            outFilename,
-            fs,
+        // read repository list
+        var (
+            scanner  *bufio.Scanner = bufio.NewScanner(refListReader)
+            resolver = blockrepository.MakeKnownFileSizedBlockResolver(int64(blocksize), filesize)
+            verifier = &filechecksum.HashVerifier{
+                Hash:                filechecksum.DefaultFileHashGenerator(),
+                BlockSize:           uint(blocksize),
+                BlockChecksumGetter: checksumLookup,
+            }
+
+            sourceList []string = nil
+            repoList   []patcher.BlockRepository = nil
         )
+        for scanner.Scan() {
+            sourceList = append(sourceList, scanner.Text())
+        }
+        err = scanner.Err()
+        if err != nil {
+            return errors.WithStack(err)
+        }
+        for rID, src := range sourceList {
+            log.Infof("%v : %v", rID, src)
+            repoList = append(repoList,
+                blockrepository.NewBlockRepositoryBase(
+                    uint(rID),
+                    blocksources.NewRequesterWithTimeout(src, time.Duration(10) * time.Second),
+                    resolver,
+                    verifier))
+        }
+        msync, err := multisources.NewMultiSourcePatcher(outFile, repoList, index)
         if err != nil {
             return errors.WithStack(err)
         }
 
-        err = rsync.Patch()
+        log.Infof("Start patching %v for the size of %v", outFileName, filesize)
+        err = msync.Patch()
         if err != nil {
             return errors.WithStack(err)
         }
 
-        return errors.WithStack(rsync.Close())
+        return errors.WithStack(msync.Close())
     })
 }
